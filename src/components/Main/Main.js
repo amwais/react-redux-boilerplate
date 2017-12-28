5c729f83c5df9fa91a211bd73124b2d1f83110ce
amirplanet <amir.waisman@planet.com>
2017-12-28 14:04:21 +0100

    Basic functionality

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,22 +2,55 @@ import React, { Component } from 'react';
 
 export default class Main extends Component {
   state = {
-    headerTitle: "Multi-purpose app"
+    value: "TITLE",
+    count: 0
   };
-
 
   componentDidMount() {
     // this.props.fetchData();
   };
 
-  render() {
-    console.log('Hello');
+  onIncrease = () => {
+    this.setState({
+      count: this.state.count + 1
+    });
+  }
 
+  onDecrease = () => {
+    this.setState({
+      count: this.state.count - 1
+    });
+  };
+
+  onReset = () => {
+    this.setState({
+      count: 0
+    });
+  };
+
+  handleChagneTitle = (e) => {
+    e.preventDefault();
+    this.setState({
+      value: e.target.elements.input.value
+    });
+
+    e.target.elements.input.value = '';
+  }
+
+  render() {
     // const { fetchSatellitesError, isFetchingSatellites, satelliteStatus } = this.props;
     
     return (
       <div>
-        <h1>{this.state.headerTitle}</h1>
+        <h1>{this.state.value}</h1>
+        <form onSubmit={this.handleChagneTitle}>
+          <input type="text" name="input"></input>
+          <button>Change Title</button>
+        </form>
+        <p>{this.state.count}</p>
+        <button onClick={this.onIncrease}>Increase</button>
+        <button onClick={this.onDecrease}>Decrease</button>
+        <button onClick={this.onReset}>Reset</button>
       </div>
     );
   }
